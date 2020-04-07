96e33c8f704d304cd38da8dcfa3d122d7210dbf4
andrioo <62404599+andrioo@users.noreply.github.com>
2020-04-06 20:52:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,105 @@
-# Week of march 23 - Lore
-# Modularity, Github, import as, 
+#imports pygame as well as other .py files, such as settings and sprites. 
+# Both of which are part of the overall gameQuest github.
 
 import pygame as pg
-from pygame.sprite import Group
-# from pg.sprite import Group
-import random
+import sys
+from os import path
 from settings import *
 from sprites import *
+from tilemap import *
 
 class Game:
     def __init__(self):
-        # initialize game window, etc
         pg.init()
-        pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        self.running = True
+        pg.key.set_repeat(500, 100)
+        self.load_data()
+ 
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'img')
+        self.map = Map(path.join(game_folder, 'map2.txt'))
+        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
 
     def new(self):
-        # start a new game
-        self.all_sprites = Group()
-        self.platforms = pg.sprite.Group()
-        self.player = Player(self)
-        self.all_sprites.add(self.player)
-        ground = Platform(0, HEIGHT-40, WIDTH, 40)
-        plat1 = Platform(200, 400, 150, 20)
-        plat2 = Platform(150, 300, 150, 20)
-        self.all_sprites.add(ground)
-        self.platforms.add(ground)
-        self.all_sprites.add(plat1)
-        self.platforms.add(plat1)
-        self.all_sprites.add(plat2)
-        self.platforms.add(plat2)
-        # for plat in range(1,10):
-        #     plat = Platform(random.randint(0, WIDTH), random.randint(0, HEIGHT), 200, 20)
-        #     self.all_sprites.add(plat)
-        #     self.platforms.add(plat)
-        self.run()
-
+        # initializes all of the variables and does all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        # code below uses map data forming a 2d array from lines
+        for row, tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col,  row)
+                if tile == 'p':
+                    self.player = Player(self, col, row)
+        print(self.map.data)
+        self.camera = Camera(self.map.width, self.map.height)
+        # for x in range(10, 20):
+        #     Wall(self, x, 6)
 
     def run(self):
-        # Game Loop
+        # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            self.clock.tick(FPS)
+            self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
+#quits
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
     def update(self):
-        # Game Loop - Update
+        # updates portion of the game loop
         self.all_sprites.update()
-        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-        if hits:
-            if self.player.rect.top > hits[0].rect.top:
-                print("i hit my head")
-                self.player.vel.y = 15
-                self.player.rect.top = hits[0].rect.bottom + 5
-                self.player.hitpoints -= 10
-                print(self.player.hitpoints)
-            # print("it collided")
-            else:
-                self.player.vel.y = 0
-                self.player.pos.y = hits[0].rect.top+1
-            
+        self.camera.update(self.player)
 
-    def events(self):
-        # Game Loop - events
-        for event in pg.event.get():
-            # check for closing window
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
-        # Game Loop - draw
-        self.screen.fill(BLACK)
-        self.all_sprites.draw(self.screen)
-        # *after* drawing everything, flip the display
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        # self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
 
+    def events(self):
+        # catches all of the events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                    # below is for step based movement
+                # if event.key == pg.K_a:
+                #     self.player.move(dx=-1)
+                # if event.key == pg.K_d:
+                #     self.player.move(dx=1)
+                # if event.key == pg.K_w:
+                #     self.player.move(dy=-1)
+                # if event.key == pg.K_s:
+                #     self.player.move(dy=1)
+
     def show_start_screen(self):
-        # game splash/start screen
         pass
 
     def show_go_screen(self):
-        # game over/continue
         pass
 
+# create the game object
 g = Game()
 g.show_start_screen()
-while g.running:
+while True:
     g.new()
+    g.run()
     g.show_go_screen()
-
-pg.quit()
