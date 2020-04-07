f4780397329db565ff55a76fc78b4bc0a86e45c7
andrioo <62404599+andrioo@users.noreply.github.com>
2020-04-07 11:54:51 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # defines an array of colors (R, G, B)
 # You can just look these up on the internet
-# added my own colors
+# Added new colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARKGREY = (40, 40, 40)
@@ -8,18 +8,18 @@ LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-# Added colors over here
-BLUE = (0,0,255)
+# Here are the new colors I have added:
 GOLD = (255,215,0)
 PINK = (255,192,203)
+BLUE = (0,0,255)
 
-# game options/settings
+# Defines size of window
 TITLE = "Jumpy!"
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
 
-# Environment options
+# Adds gravity
 GRAVITY = 9.8
 
 # Player properties
