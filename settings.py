f194f9698054bcb5f94874ba937ca61d89fdf1a4
andrioo <62404599+andrioo@users.noreply.github.com>
2020-04-06 20:54:04 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,17 +1,23 @@
-# game options/settings
-TITLE = "Jumpy!"
-WIDTH = 480
-HEIGHT = 600
-FPS = 60
-
-# Player properties
-PLAYER_ACC = 0.5
-PLAYER_FRICTION = -0.12
-
-# define colors
+# defines an array of colors (R, G, B)
+# You can just look these up on the internet
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+PLAYER_SPEED = 300
+PLAYER_IMG = 'hero.png'
