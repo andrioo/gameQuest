131f97de18dc7beeae56ef94b2b2cdd468a5ad39
andrioo <62404599+andrioo@users.noreply.github.com>
2020-04-06 20:55:11 -0700

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,37 +1,105 @@
-# Sprite classes for platform game
+#sprites are created here
+
 import pygame as pg
-from pg.sprite import Sprite
 from settings import *
 vec = pg.math.Vector2
 
-class Player(Sprite):
-    def __init__(self):
-        Sprite.__init__(self)
-        self.image = pg.Surface((30, 40))
-        self.image.fill(YELLOW)
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        # self.image = pg.Surface((TILESIZE, TILESIZE))
+        # self.image.fill(YELLOW)pos.
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0,0)
+        # self.pos = vec(0,0)
+        self.pos = vec(x, y) * TILESIZE
+        # self.vx, self.vy = 0, 0
+        # self.x = x * TILESIZE
+        # self.y = y * TILESIZE
+    # below is for smoother movement 
+    def get_keys(self):
+        # self.vx, self.vy = 0, 0
         self.vel = vec(0, 0)
-        self.acc = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vel.x = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vel.x = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vel.y = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vel.y = PLAYER_SPEED
+        if self.vel.x != 0 and self.vel.y != 0:
+            # self.vx * 0.7071
+            # self.vy * 0.7071
+            # self.vx /= 1.414
+            # self.vy /= 1.414
+            self.vel *= 0.7071
+    # legacy below...
+    # def move(self, dx=0, dy=0):
+    #     if not self.collide_with_walls(dx, dy):
+    #         self.x += dx
+    #         self.y += dy
+
+    # def collide_with_walls(self, dx=0, dy=0):
+    #     for wall in self.game.walls:
+    #         if wall.x == self.x + dx and wall.y == self.y + dy:
+    #             print("collided...")
+    #             return True
+    #     return False
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
+
 
     def update(self):
-        self.acc = vec(0, 0)
-        keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT]:
-            self.acc.x = -PLAYER_ACC
-        if keys[pg.K_RIGHT]:
-            self.acc.x = PLAYER_ACC
+        # below is for smoother movement
+        self.get_keys()
+        self.pos += self.vel * self.game.dt
+        # self.x += self.vx * self.game.dt
+        # self.y += self.vy * self.game.dt
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+        # self.rect.topleft = (self.x, self.y)
+        
+        # below is a bad way to do collisions
+        # if pg.sprite.spritecollideany(self, self.game.walls):
+        #     self.x -= self.vx * self.game.dt
+        # below is for step based movement
+        # self.rect.x = self.x * TILESIZE
+        # self.rect.y = self.y * TILESIZE
 
-        # apply friction
-        self.acc += self.vel * PLAYER_FRICTION
-        # equations of motion
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-        # wrap around the sides of the screen
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
 
-        self.rect.center = self.pos
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
